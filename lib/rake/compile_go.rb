adfcefd03b96b80ea907b5a13cc051cdee4775f5
Olivier Albiez <olivier.albiez@poloper.org>
2014-12-02 21:32:39 +0100

    Go::compile removed

--- lib/rake/compile_go.rb
+++ lib/rake/compile_go.rb
@@ -1,34 +1,6 @@
 require 'fileutils'
 
 module Go
-    def self.compile(options={})
-        goversion = options[:goversion] || "1.3.3"
-        repository = options[:repository]
-        tag = options[:tag] || "master"
-        package = options[:package]
-        workspace = options[:workspace]
-        target = options[:target]
-        static = options[:static] || false
-        strip = options[:strip] || false
-        path = File.join(workspace, 'src', options[:path] || package)
-        if Dir.exists?(path)
-            Dir.chdir(path) { system 'git', 'fetch', '--update-head-ok' }
-        else
-            system 'git', 'clone', repository, path
-        end
-        Dir.chdir(path) { system 'git', 'checkout', '-q', '-f', tag }
-        go_command = ['docker', 'run', '--rm', '-v', "#{workspace}:/go"]
-        go_command << '-e' << 'CGO_ENABLED=0' if static
-        go_command << "golang:#{goversion}" << 'go' << 'get'
-        go_command << '-a' << '--ldflags' << '-s -extldflags \"-static\"' if static
-        go_command << package
-        system *go_command
-        Dir.glob(File.join(workspace, 'bin', '*')).each do |binary|
-            system 'strip', '--strip-debug', binary  if strip
-            FileUtils.cp binary, target
-        end
-    end
-
     def self.compilation(options={}, &block)
         workspace = options[:workspace] || File.join(Dir.pwd, 'go')
         goversion = options[:goversion] || "1.3.3"
